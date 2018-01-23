fb26745e1fb4d7efce78f3be6748c90087b2c729
Andy Teucher <andy.teucher@gov.bc.ca>
2018-01-22 17:04:37 -0800

    Initial start on getting data from web

---
+++ R/metadata.R
@@ -0,0 +1,37 @@
+# Copyright 2018 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+get_wq_sites <- function() {
+  ret <- get_metadata_file("Water-Qual-Eau-Sites-National")
+  # Get rid of weird encoding artifacts
+  for (i in seq_len(ncol(ret))) {
+    if (is.character(ret[[i]])) {
+      ret[[i]] <- gsub("\u0096", "-", ret[[i]])
+    }
+  }
+  ret
+}
+
+get_wq_params <- function() {
+  get_metadata_file("Water-Qual-Eau-VariableInfo")
+}
+
+get_wq_param_desc <- function() {
+  get_metadata_file("Water-Qual-Eau-TableDescriptions")
+}
+
+get_basin <- function(site_no) {
+  sites <- get_wq_sites()
+  basin <- sites$PEARSEDA[sites$SITE_NO == site_no][1]
+  basin
+  
+}
---
+++ R/utils.R
@@ -0,0 +1,67 @@
+# Copyright 2018 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+safe_make_url <- function(...) {
+  parts <- list(...)
+  parts <- gsub("^/+|/+$", "", parts)
+  paste(parts, collapse = "/")
+}
+
+base_url <- function() {
+  "http://data.ec.gc.ca/data/substances/monitor/national-long-term-water-quality-monitoring-data"
+}
+
+get_metadata_json <- function() {
+  url <- safe_make_url(base_url(), "datapackage.json")
+  res <- httr::GET(url)
+  httr::stop_for_status(res)
+  httr::content(res, as = "parsed", type = "application/json")
+}
+
+get_resources_df <- function() {
+  resources <- get_metadata_json()[["resources"]]
+  dplyr::bind_rows(resources)
+}
+
+get_metadata_file <- function(name) {
+  resources <- get_resources_df()
+  resource <- resources[resources$name == name, ]
+  if (nrow(resource) == 0) 
+    stop("No resources found matching that name", call. = FALSE)
+  if (nrow(resource) > 1) 
+    stop("More than one resource found matching that name", call. = FALSE)
+  
+  url <- safe_make_url(base_url(), resource$path)
+  x <- tempfile()
+  res <- httr::GET(url, httr::write_disk(x))
+  httr::stop_for_status(res)
+  # cont <- httr::content(res, as = "raw", type = resource$mediatype)
+  parse_ec(x, resource$format)
+}
+
+parse_ec <- function(x, mime_type) {
+  switch(mime_type, 
+         csv = readr::read_csv(x, locale = readr::locale(encoding = "latin1")))
+}
+
+basin_folders <- function() {
+  ec_site <- xml2::read_html("http://data.ec.gc.ca/data/substances/monitor/national-long-term-water-quality-monitoring-data/?lang=en")
+  link_tbl <- ec_site %>% 
+    rvest::html_node("#indexlist") %>% 
+    rvest::html_table()
+  
+  link_tbl <- link_tbl[link_tbl$Size == "-" & link_tbl$Name != "Parent Directory", 
+                       c("Name", "Last modified")]
+  link_tbl$short_name <- gsub("(-river)?(-basin)?-long-term-water-quality-monitoring-data", 
+                              "", link_tbl$Name)
+  link_tbl
+}
