8c908be0406edb717f1c7e65ec1946ac1b634879
Andy Teucher <andy.teucher@gov.bc.ca>
2018-01-25 17:21:58 -0800

    Document internal functions

--- R/utils.R
+++ R/utils.R
@@ -10,8 +10,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+#' Combine parts of a url into one (not queries etc just paths)
+#'
+#' @param ... parts of the url
+#'
+#' @return a single url
+#' @noRd
 safe_make_url <- function(...) {
   parts <- compact(list(...))
+  # Get rid of leading and lagging slashes, then combine parts with slashes
   parts <- gsub("^/+|/+$", "", parts)
   paste(parts, collapse = "/")
 }
@@ -20,6 +27,12 @@ base_url <- function() {
   "http://data.ec.gc.ca/data/substances/monitor/national-long-term-water-quality-monitoring-data/"
 }
 
+#' Get the 'datapackage.json file from a given folder and parse it 
+#' into a list. 
+#'
+#' @param folder the name of the folder (subfolder of base_url)
+#'
+#' @noRd
 get_metadata_json <- function(folder = NULL) {
   url <- safe_make_url(base_url(), folder, "datapackage.json")
   res <- httr::GET(url)
@@ -27,11 +40,21 @@ get_metadata_json <- function(folder = NULL) {
   httr::content(res, as = "parsed", type = "application/json")
 }
 
+#' Extract the resources from a datapackage.json in a given folder
+#'
+#' @param folder the name of the folder (subfolder of base_url)
+#'
+#' @noRd
 get_resources_df <- function(folder = NULL) {
   resources <- get_metadata_json(folder)[["resources"]]
   dplyr::bind_rows(resources)
 }
 
+#' Get a 
+#'
+#' @param name 
+#'
+#' @return a data frame of metadata
 get_metadata_file <- function(name) {
   resources <- get_resources_df()
   resource <- resources[resources$name == name, ]
@@ -47,6 +70,15 @@ get_metadata_file <- function(name) {
            resource$format)
 }
 
+#' Parse EC metadata
+#' 
+#' Probably overkill to make this function, but
+#' just in case they switch to something other than csv
+#'
+#' @param x 
+#' @param mime_type 
+#'
+#' @noRd
 parse_ec <- function(x, mime_type) {
   switch(mime_type, 
          csv = suppressMessages(
@@ -54,6 +86,8 @@ parse_ec <- function(x, mime_type) {
            )
 }
 
+#' Find the links to the folders that contain the csvs for the basins.
+#' @noRd
 basin_folders_ <- function() {
   ec_site <- xml2::read_html(base_url())
   link_tbl <- rvest::html_node(ec_site, "#indexlist")
@@ -66,6 +100,12 @@ basin_folders_ <- function() {
 
 basin_folders <- memoise::memoise(basin_folders_)
 
+#' Given a basin name (ideally from wq_sites()$PEARSEDA) get the url 
+#' for the folder that contains the data
+#'
+#' @param basin 
+#'
+#' @noRd
 basin_url <- function(basin) {
   basin_links <- basin_folders()
   basin_links_clean <- clean_names(basin_links[["name"]])
@@ -88,11 +128,21 @@ clean_names <- function(x) {
 
 compact <- function(x) Filter(Negate(is.null), x)
 
+
+#' Read a csv from the website. 
+#'
+#' @param x 
+#'
+#' @noRd
 read_canwq_csv <- function(x) {
+  # Read just first row to get the names and standardize - on some of them the 
+  # UNIT_UNITE column has accent aigu. Could probaly just use a character vector
+  # but that will break if the column order ever changes
   nms <- names(suppressMessages(
     readr::read_csv(x, n_max = 1, locale = readr::locale(encoding = "latin1"))
   ))
   nms[grepl("^UNIT_", nms)] <- "UNIT_UNITE"
+  
   ret <- readr::read_csv(x, 
                          locale = readr::locale(encoding = "latin1"), 
                          col_types = readr::cols(
