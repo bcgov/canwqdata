f97ac5c78caa35790942b3de9a11d83baf60360a
Andy Teucher <andy.teucher@gov.bc.ca>
2018-01-26 13:28:27 -0800

    Document

---
+++ R/canwqdata-package.r
@@ -0,0 +1,9 @@
+#' Get Canadian Long-Term Water Quality Monitoring Data
+#' 
+#' All data is freely available under the 
+#' [Open Government License Canada](http://open.canada.ca/en/open-government-licence-canada) 
+#' from [here](http://data.ec.gc.ca/data/substances/monitor/national-long-term-water-quality-monitoring-data/).
+#'
+#' @name canwqdata
+#' @docType package
+NULL
